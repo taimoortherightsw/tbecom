a85bb3a97897ca6cfa8a521c960fc798e4c4ecee
Taimoor Ali <taimoor.ali@therightsw.com>
2021-09-30 19:06:56 +0500

    added governorate and area UI templates

--- assets/js/app-script.js
+++ assets/js/app-script.js
@@ -10,6 +10,7 @@ EcwidApp.getAppStorage('installed', function (config) {
 			public: {
 				enabled: true,
 				governorates: [],
+				areas: {}
 			},
 			private: {
 				installed: 'true',
@@ -34,6 +35,7 @@ let AppStorage = {
 		var config = {
 			enabled: true,
 			governorates: [],
+			areas: {},
 		};
 
 		EcwidApp.setAppPublicConfig(JSON.stringify(config), function(updatedConfig){
@@ -50,7 +52,7 @@ let AppStorage = {
 			else $('#ta__app-status').removeAttr('checked');
 
 			for (key in config.governorates) {
-				_self.createUI('governorate', config.governorates[key]);
+				_self.createGovernorateUI(config.governorates[key]);
 			}
 		});
 	},
@@ -88,36 +90,96 @@ let AppStorage = {
 			});
 		});
 	},
-	createUI: function (type, data) {
-		if (type == 'governorate') {
-			let governorateTemplate = `
-				<div class="list-element list-element--has-hover list-element--compact list-element--inline-mode">
+	createGovernorateUI: function (governorate) {
+		let governorateTemplate = `
+			<div class="list-element list-element--has-hover list-element--compact list-element--inline-mode">
+				<div class="list-element__content">
+					<div class="list-element__info">
+						<div class="list-element__data-row">
+							<span class="text-default">
+								<b><span class="gwt-InlineLabel">${governorate}</span></b>
+							</span>
+						</div>
+					</div>
+					<div class="list-element__actions">
+						<label class="checkbox" style="padding-right: 90px; margin-top: 4px;">
+							<input type="checkbox" class="ta__data-switch" data-type="governorate" data-value="${governorate}">
+							<div data-on="enabled" data-off="disabled"><div></div></div>
+						</label>
+						<div class="list-element__buttons-set">
+							<div class="list-element__button-wrapper">
+								<button type="button" class="btn btn-default btn-small ta__delete-btn" data-type="governorate" data-value="${governorate}">
+									Delete
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`;
+
+		$('#ta__governorates-container').append(governorateTemplate);
+
+		let areaGovernorateTemplate = `
+			<span data-areas-for="${governorate}">
+				<div class="list-element list-element--compact list-element--inline-mode" style="border-top: none;">
 					<div class="list-element__content">
 						<div class="list-element__info">
 							<div class="list-element__data-row">
 								<span class="text-default">
-									<b><span class="gwt-InlineLabel">${data}</span></b>
+									<h3>${governorate}</h3>
 								</span>
 							</div>
 						</div>
 						<div class="list-element__actions">
-							<label class="checkbox" style="padding-right: 90px; margin-top: 4px;">
-								<input type="checkbox" class="ta__data-switch" data-type="governorate" data-value="${data}">
-								<div data-on="enabled" data-off="disabled"><div></div></div>
-							</label>
 							<div class="list-element__buttons-set">
 								<div class="list-element__button-wrapper">
-									<button type="button" class="btn btn-default btn-small ta__delete-btn" data-type="governorate" data-value="${data}">
-										Delete
+									<button type="button" class="btn btn-primary btn-small ta__add-btn" data-type="area" data-governorate="${governorate}">
+										<span class="svg-icon">
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" focusable="false">
+												<path d="M6.712 5.314H11v1.39H6.712V11H5.267V6.706H1V5.314h4.267V1h1.446v4.314z"></path>
+											</svg>
+										</span>
+										New
 									</button>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			`;
+			</span>
+		`;
 	
-			$('#ta__governorates-container').append(governorateTemplate);
-		}
+		$('#ta__areas-container').append(areaGovernorateTemplate);
+	},
+	createAreaUI: function (governorate, area) {
+		let areaTemplate = `
+			<div class="list-element list-element--has-hover list-element--compact list-element--inline-mode">
+				<div class="list-element__content">
+					<div class="list-element__info">
+						<div class="list-element__data-row">
+							<span class="text-default">
+								<b><span class="gwt-InlineLabel">${area}</span></b>
+							</span>
+						</div>
+					</div>
+					<div class="list-element__actions">
+						<label class="checkbox" style="padding-right: 90px; margin-top: 4px;">
+							<input type="checkbox" class="ta__data-switch" data-type="area" data-value="${area}">
+							<div data-on="enabled" data-off="disabled"><div></div></div>
+						</label>
+						<div class="list-element__buttons-set">
+							<div class="list-element__button-wrapper">
+								<button type="button" class="btn btn-default btn-small ta__delete-btn" data-type="area" data-value="${area}" data-governorate="${governorate}">
+									Delete
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		`;
+
+		$('span[data-areas-for="' + governorate +'"]').append(areaTemplate);
 	}
 }
